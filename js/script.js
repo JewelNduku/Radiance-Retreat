2314e1805d1745df3defeb4e4087200d01d56f1d
JewelNduku <nduku.joan@strathmore.edu>
2026-06-23 19:58:36 +0300

    created js folder

---
+++ js/script.js
@@ -0,0 +1,16 @@
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let message = document.getElementById("message").value;
+
+    if (name === "" || email === "" || message === "") {
+
+        event.preventDefault();
+
+        document.getElementById("errorMessage").innerHTML =
+            "Please fill in all fields.";
+
+        document.getElementById("errorMessage").style.color = "#b94f8a";
+    }
+});
